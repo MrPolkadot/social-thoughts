52e0e1e3757af975f9c04871547424013957a88f
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-10-18 21:50:53 -0600

    created file structure.

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+
+const PORT = process.env.PORT || 2099;
+const app = express();
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json);
+app.use(routes);
+
+db.once("Open", () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on http://localhost:${PORT}`)
+    });
+});
