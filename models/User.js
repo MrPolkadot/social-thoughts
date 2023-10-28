a2decd31c8a905825815df34d8d17cb11685c912
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-10-27 19:37:08 -0600

    Added virtual on userSchema

--- models/User.js
+++ models/User.js
@@ -37,8 +37,14 @@ const userSchema = new Schema(
     }
 )
 
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+});
 
 const User = model("user", userSchema);
 //Create virtual called friendCount that retrieves the length of the user's friends array field on query.
+
+
+
 
 module.exports = User;
