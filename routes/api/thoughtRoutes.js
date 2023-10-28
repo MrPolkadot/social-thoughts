6cfc8ec8e174b007f21b963a5b3f0c2371193f3c
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-10-27 20:10:00 -0600

    added routes into thoughtRoutes.js

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,8 +1,30 @@
 const router = require("express").Router();
 
+const {
+    allThoughts,
+    singleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction,
+} = require("../../controllers/thoughtController.js");
 
+// /api/thoughts
+router.route("/").get(allThoughts).post(createThought);
 
+// /api/thoughts/:thoughtId
+router
+    .route("/:thoughtId")
+    .get(singleThought)
+    .put(updateThought)
+    .delete(deleteThought)
 
+// /api/thoughts/:thoughtId/reactions
+router
+    .route("/:thoughtId/reactions")
+    .post(createReaction)
+    .delete(deleteReaction)
 
 
 module.exports = router;
